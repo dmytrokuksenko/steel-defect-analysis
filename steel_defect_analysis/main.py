96e12059e736a12ebbcc6c87c6a0c48053960bda
unknown <kuksenko.dima@gmail.com>
2022-09-23 16:02:05 -0600

    added visualization of random images -> sanity check

--- steel_defect_analysis/main.py
+++ steel_defect_analysis/main.py
@@ -1,1 +1,35 @@
-"main file for the project" 
+"""
+    Classification of defects in hot rolled surface steel. 
+
+    @author: Dmytro Kuksenko
+    @date: Sept 23, 2022
+
+    References:
+    
+    [1] Cohn, R., Holm, E. Unsupervised Machine Learning aia Transfer Learning 
+    and k-Means Clustering to Classify Materials Image Data. 
+    Integr Mater Manuf Innov 10, 231–244 (2021). 
+    https://doi.org/10.1007/s40192-021-00205-8
+"""
+
+import os
+from process import get_images
+from plot import plot_images
+
+if __name__ == "__main__":
+    
+    # set path to a data dir
+    data_dir = 'data'
+
+    # extract images
+    class_names = sorted([x for x in os.listdir(data_dir)])
+
+    image_files = get_images(data_dir, class_names)
+    total_im_num = sum([len(x) for x in image_files])
+
+    for name, files in zip(class_names, image_files):
+        print(f'Class {name} contains {len(files)} images')
+
+    print(f'Total number of images is {total_im_num}')
+
+    plot_images(image_files, class_names)
--- steel_defect_analysis/plot.py
+++ steel_defect_analysis/plot.py
@@ -5,4 +5,24 @@
     @date: Sept 23, 2022
 """
 
+import numpy as np
 import matplotlib.pyplot as plt
+import skimage
+
+
+def plot_images(image_files, class_names, im_plot=9):
+    
+    plt.subplots(3,3, figsize=(8,8))
+    plt.title('Visualization of randomly selected images')
+
+    for i in range(im_plot):
+        class_idx = np.random.randint(len(class_names))
+        img_idx = np.random.randint(len(image_files[class_idx]))
+        im = skimage.io.imread(image_files[class_idx][img_idx], as_gray=True)
+        plt.subplot(3,3,i+1)
+        plt.xlabel(class_names[class_idx])
+        plt.imshow(im, cmap='gray')
+    
+    plt.tight_layout()
+    plt.show()
+
